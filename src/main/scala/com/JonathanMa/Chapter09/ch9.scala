a58d5669088adae5b889d6b6426f085ceb017cbf
Lee KeeBum <jma66@hawaii.edu>
2023-12-07 22:46:28 -1000

    finished 9.3

--- src/main/scala/com/JonathanMa/Chapter09/ch9.scala
+++ src/main/scala/com/JonathanMa/Chapter09/ch9.scala
@@ -1,8 +1,9 @@
 package com.JonathanMa.Chapter09
+
 import java.io.File
 
 
-object FileMatcher{
+object FileMatcher {
   private val files = new java.io.File(".").listFiles
 
   def fileEnding(query: String): Array[File] = for (file <- files; if file.getName.endsWith(query))
@@ -17,15 +18,26 @@ object FileMatcher{
 
 }
 
-object FileMatcher2{
+object FileMatcher2 {
   private val files = new File(".").listFiles()
 
-  private def fileMatcher(query: String, method: (String, String) => Boolean): Array[File] = for (file <- files; if method(file.getName, query))
+  private def fileMatcher(method: String => Boolean): Array[File] = for (file <- files; if method(file.getName))
     yield file
 
-  def fileRegex(query: String): Array[File] = fileMatcher(query, _.matches(_))
+  def fileRegex(query: String): Array[File] = fileMatcher(_.matches(query))
 
-  def fileContain(query: String): Array[File] = fileMatcher(query, _.contains(_))
+  def fileContain(query: String): Array[File] = fileMatcher(_.contains(query))
 
-  def fileEnds(query: String): Array[File] = fileMatcher(query, _.endsWith(_))
+  def fileEnds(query: String): Array[File] = fileMatcher(_.endsWith(query))
+}
+
+object Section2{
+  def containsNeg(nums: List[Int]): Boolean = nums.exists(_ < 0)
+  def containsOdd(nums: List[Int]): Boolean = nums.exists(_ % 2 == 1)
+}
+
+object Section3{
+  def curriedSum(x: Int)(y: Int): Int = x + y
+
+  def onePlus: Int => Int = curriedSum(1) _
 }

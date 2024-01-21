1b96576ec4474f2721d0d5ad1695cc9aeea47bf2
Lee KeeBum <jma66@hawaii.edu>
2024-01-21 08:14:04 -0500

    wip

--- src/main/scala/com/JonathanMa/Chapter14/ch14.scala
+++ src/main/scala/com/JonathanMa/Chapter14/ch14.scala
@@ -6,7 +6,7 @@ package com.JonathanMa.Chapter14
  */
 object Chapter14{
   def addPos(x: Int, y:Int): Int = {
-
+    assert(x > 0 && y > 0, "Must be positive numbers")
     x + y
   }
 
@@ -22,6 +22,17 @@ object Chapter14{
   14.2 Testing in Scala
     There are many options to test in Scala from established Java tools such as JUnit and TestNG to tools written in
     Scala, such as ScalaTest, specs2, and ScalaCheck.
-    We'll start with ScalaTest.
  */
 
+/*
+  14.3 Informative failure reports
+    We can get more descriptive error messages using Diagrams trait. If you wish to emphasize the distinction between actual and expected,
+    you can use assertResult method.
+
+    We can also check to see if the exceptions thrown is as expected with assertThrows[what you expect].
+ */
+
+/*
+  14.4 Tests as specifications
+
+ */
--- src/test/scala/Chapter14.scala
+++ src/test/scala/Chapter14.scala
@@ -1,10 +1,25 @@
 import org.scalatest.funsuite.AnyFunSuite
 import com.JonathanMa.Chapter14.Chapter14._
+import org.scalatest.diagrams.Diagrams
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class Chapter14 extends AnyFunSuite {
+class AnyFun extends AnyFunSuite {
+  test("addPos correctly adds two positive numbers"){
+    assertResult(4)(addPos(1, 3))
+  }
   test("addPos should throw an AssertionError for non-positive inputs"){
     assertThrows[AssertionError] {
       addPos(1, -2)
     }
+  }
+}
+
+class AnyFlat extends AnyFlatSpec with Matchers{
+  "addPos method" should "correctly add two integers" in {
+    addPos(1, 3) should be (4)
+  }
+  it should "throw AssertException if int provided is negative" in {
+    an [AssertionError] should be thrownBy { addPos(1, -3) }
   }
 }

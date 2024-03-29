61a60633105e31ac1a60075c9ec580e41b4e2c4e
Lee KeeBum <jma66@hawaii.edu>
2024-03-28 21:29:06 -0400

    on 16.7 wip

--- src/main/scala/com/JonathanMa/Chapter16/ch16.scala
+++ src/main/scala/com/JonathanMa/Chapter16/ch16.scala
@@ -1,3 +1,5 @@
+import Chapter16Lists.fruits
+
 object Chapter16Lists {
 /*
   Chapter 16: Working with Lists
@@ -11,8 +13,7 @@ object Chapter16Lists {
   // 16.3 Constructing lists and list literals
 
   val fruits: List[String] = "lemon" :: ("orange" :: ("watermelon" :: Nil))
-  val nums: List[Int] = 1 :: (2 :: (3 :: Nil))
-  val diag: List[List[Int]] = 1 :: (0 :: (0 :: Nil)) :: (0 :: (1 :: (0 :: Nil))) :: (0 :: (0 :: (1 :: Nil))) :: Nil
+  val nums: List[Int] = 1 :: (2 :: (3 :: (4 ::Nil)))
   // We can leave out the parentheses:
   val reg: List[String] = "hello" :: "there" :: "!" :: Nil
 
@@ -35,15 +36,54 @@ object Chapter16Lists {
   // j = 1 and rest is List(1, 2)
   val j :: rest = nums
 
-  /*16.6 Concatenating two lists*/
-  //example: List(1, 2) ::: List(3, 4, 5) -> List(1, 2, 3, 4, 5)
-  /*
-    Or we can make a create an append function because its more instructive.
-   */
-  def append(xs: List[T], ys: List[T]):List[T] =
-    xs match {
-      case List() => ys
-      case x :: xs1 => x :: append(xs1, ys)
-    }
-  }
+  /*16.6 First order methods on class List*/
 
+  // Taking the length of a list
+  val length: Int = fruits.length
+
+  // Accessing the end of a list: init and last
+  val abcde: List[Char] = List('a', 'b', 'c', 'd', 'e')
+  // abcde.last returns 'e' and abcde.init returns List(a, b, c, d)
+
+  // Reversing lists: abcde.reverse -> List(e, d, c, b, a)
+  // Reverse can be implemented using concatenation (:::):
+  def rev[T](xs: List[T]): List[T] = xs match {
+    case List() => xs
+    case x :: xs1 => rev(xs1) ::: List(x)
+  }
+  /* Following will be in test suites */
+  /* Prefixes and suffixes: drop, take, splitAt */
+  // spliAt returns two lists that is split at n
+  // take returns a list of first n elements
+  // drop returns list of elements after n
+  //abcde spliAt 2 equals abcde take 2, abcde drop 2
+
+  /* Element selection: apply and indices */
+  // apply is basically element selection: abcde(3) is the same as abcde.apply(3)
+  // indices just returns a range of the number of elements: abcde.indices -> Range(0, 1, 2, 3, 4)
+
+  /* Flattening a list of lists: flatten */
+  // List(List(1, 2), List(3), List(), List(4, 5)).flatten -> List(1, 2, 3, 4, 5)
+
+  /* Zipping lists: zip and unzip */
+  // abcde.indices zip abcde -> Vector((0,a), (1,b), (2,c), (3,d), (4,e))
+  // the above can also be done with zipWithIndex: abcde.zipWithIndex
+  // Any list of tuples can be unzipped back into a tuple of lists by using unzip method
+  // abcde.zipWithIndex.unzip -> List(List('a', 'b', 'c', 'd', 'e'), List(0, 1, 2, 3, 4))
+
+  /* Displaying lists: toString and mkString */
+  // abcde.toString -> String = List(a, b, c, d, e)
+  // abcde.mkString("[", ",", "]") -> String = [a, b, c, d, e]
+
+  /* Converting lists: iterator, toArray, copyToArray */
+  // abcde.toArray -> Array(a, b, c, d, e)
+
+  /* 16.7 Higher-order methods on class List */
+  /* Mapping over lists: map, flatmap and foreach */
+  // val nums = List(1, 2, 3, 4)
+  // nums.map(_*2) -> List(2, 4, 6, 8)
+  val words = List("the", "quick", "brown", "fox")
+  // words.map(_.length) -> List(3, 5, 5, 3)
+
+
+}
--- src/test/scala/com/JonathanMa/Chapter16/Chapter16Test.scala
+++ src/test/scala/com/JonathanMa/Chapter16/Chapter16Test.scala
@@ -1,9 +1,63 @@
-import Chapter16Lists.isort
+import Chapter16Lists._
 //import com.JonathanMa.Chapter16.ch16.Chapter16Lists._
 import org.scalatest.funsuite.AnyFunSuite
 
 class Chapter16Test extends AnyFunSuite {
   test("Method isort should return an empty list when provided one"){
     assert(isort(List()) == List())
+  }
+  test("Length of fruits should equal 3") {
+    assert(fruits.length == 3)
+  }
+  test("abcde.last should return 'e' "){
+    assert(abcde.last == 'e')
+  }
+  test("abcde.init should return 'List(a, b, c, d)' "){
+    assert(abcde.init == List('a', 'b', 'c', 'd'))
+  }
+  test("abcde.reverse should return 'List('e', 'd', 'c', 'b', 'a')' "){
+    assert(abcde.reverse == List('e', 'd', 'c', 'b', 'a'))
+  }
+  test("rev(List('a', 'b', 'c') should return 'List('c', 'b', 'a')' "){
+    assert(rev(List('a', 'b', 'c')) == List('c', 'b', 'a'))
+  }
+  test("abcde take 2 should return List('a', 'b')"){
+    assert(abcde.take(2) == List('a', 'b'))
+  }
+  test("abcde drop 2 should return List('c')"){
+    assert(abcde.drop(2) == List('c', 'd', 'e'))
+  }
+  test("abcde.apply(2) should equal abcde(2)"){
+    assert(abcde.apply(2) == abcde(2))
+  }
+  test("abcde.indices should return Range(0, 1, 2, 3, 4)"){
+    assert(abcde.indices == (0 to 4))
+  }
+  test("abcde.zip(List(0, 1, 2)) should return Vector(('a', 0), ('b', 1), ('c', 2))"){
+    assert(abcde.zip(List(0, 1, 2)) == Vector(('a', 0), ('b', 1), ('c', 2)))
+  }
+  test("abcde.zipWithIndex should return Vector(('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4))"){
+    assert(abcde.zipWithIndex == Vector(('a', 0), ('b', 1), ('c', 2), ('d', 3), ('e', 4)))
+  }
+  test("abcde.zipWithIndex.unzip should return List((List('a', 'b', 'c', 'd', 'e'), List(0, 1, 2, 3, 4))"){
+    assert(abcde.zipWithIndex.unzip == ((List('a', 'b', 'c', 'd', 'e'), List(0, 1, 2, 3, 4))))
+  }
+  test("abcde.toString should return \"List(a, b, c, d, e)\""){
+    assert(abcde.toString == "List(a, b, c, d, e)" )
+  }
+  test("abcde.mkString(\"[\", \",\", \"]\") should return String = [a, b, c, d, e]"){
+    assert(abcde.mkString("[", ", ", "]") == "[a, b, c, d, e]")
+  }
+
+  /* 16.7 Higher-order methods on class List */
+  test("nums.map(_ * 2) should return List(2, 4, 6, 8)"){
+    assert(nums.map(_ * 2) == List(2, 4, 6, 8))
+  }
+  test("words.map(_.length) should return List(3, 5, 5, 3)"){
+    assert(words.map(_.length) == List(3, 5, 5, 3))
+  }
+  test("words.flatMap(_.toList) should return List(t, h, e, q, u, i, c, k, b, r, o, w, n, f, o, x)"){
+    assert(words.flatMap(_.toList) == List('t', 'h', 'e', 'q', 'u', 'i', 'c', 'k', 'b', 'r', 'o', 'w',
+      'n', 'f', 'o', 'x'))
   }
 }
